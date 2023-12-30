687758600d5a31886cc1109efc7dc5369fd83d2f
Akotowicz <nekozawa33umehito@gmail.com>
2023-12-30 16:25:54 +0100

    lab 8

---
+++ lab8/Zad4.py
@@ -0,0 +1,27 @@
+from diffusers import AutoPipelineForText2Image, StableDiffusionPipeline, UNet2DConditionModel
+import torch
+torch.cuda.empty_cache()
+
+print("Torch version:",torch.__version__)
+
+print("Is CUDA enabled?",torch.cuda.is_available())
+
+# load pipeline
+model_id = "runwayml/stable-diffusion-v1-5"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+
+# load finetuned model
+unet_id = "mhdang/dpo-sd1.5-text2image-v1"
+unet = UNet2DConditionModel.from_pretrained(unet_id, subfolder="unet", torch_dtype=torch.float16)
+pipe.unet = unet
+pipe = pipe.to("cuda")
+
+prompt = "Photorealistic mountain view with forest and deer"
+image = pipe(prompt, guidance_scale=7.5).images[0].resize((512,512))
+
+image.save("zad4.2.png")
+# # show image
+# import matplotlib.pyplot as plt
+# plt.imshow(image)
+
+# paczka ma 5GB
